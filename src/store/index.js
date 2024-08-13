194e6d9941b2154d5c0a81fb14299dd353178cdc
Peter Brock <peter.brock@eviden.com>
2024-08-13 10:55:33 +0100

    Initial commit

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import { configureStore, createSlice, createAction } from "@reduxjs/toolkit";
+import { songsReducer, addSong, removeSong } from "./slices/songsSlice";
+import { moviesReducer, addMovie, removeMovie } from "./slices/moviesSlice";
+import { reset } from "./actions";
+
+const store = configureStore({
+  reducer: {
+    songs: songsReducer,
+    movies: moviesReducer
+  }
+});
+
+export { store };
+export { reset };
+export { addSong, removeSong };
+export { addMovie, removeMovie };
